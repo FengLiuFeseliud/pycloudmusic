3d2971fb38b11b6859d3fdd384c7ca2933f0b23e
sakura <1718089268@qq.com>
2022-07-28 13:38:21 +0800

    Page.all()

--- pycloudmusic/tools.py
+++ pycloudmusic/tools.py
@@ -1,5 +1,6 @@
 from typing import Callable, Optional
 from math import ceil
+import asyncio
 
 
 class Page:
@@ -20,23 +21,41 @@ class Page:
     def __aiter__(self):
         return self
 
+    async def _set_index(self, page_data):
+        try:
+            count, data = page_data
+        except ValueError:
+            return page_data
+
+        self.__max_page = ceil(count / self.limit) - 1
+        return data
+
+
     async def __anext__(self):
         if self.__page > self.__max_page:
             raise StopAsyncIteration
 
         data = await self.__api_fun(page=self.__page, limit=self.limit, **self.kwargs)
-        try:
-            count, page_data = data
-        except ValueError:
-            self.__page += 1
-            return data
-            
-        self.__max_page = ceil(count / self.limit) - 1
         self.__page += 1
-        return page_data
+        return await self._set_index(data)
 
     def set_page(self, page: int):
         if page > self.__max_page:
             raise IndexError("page > self.__max_pag")
 
         self.__page = page
+
+    async def all(self, call_fun: Optional[Callable[[dict], int]] = None):
+        data = await self.__api_fun(page=self.__page, limit=self.limit, **self.kwargs)
+        if not call_fun is None:
+            self.__max_page = call_fun(data)
+        else:
+            await self._set_index(data)
+
+        tasks, page = [], 0
+        while page <= self.__max_page:
+            tasks.append(asyncio.create_task(self.__api_fun(page=self.__page, limit=self.limit, **self.kwargs)))
+            page += 1
+        
+        done, _ = await asyncio.wait(tasks)
+        return (task.result() for task in done)
